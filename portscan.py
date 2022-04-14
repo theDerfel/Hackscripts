c7b9fd8b68c95c2b6fa201837edc30893f37532a
theDerfel <42454150+theDerfel@users.noreply.github.com>
2022-04-13 21:01:36 -0400

    Update portscan.py

--- portscan.py
+++ portscan.py
@@ -3,7 +3,7 @@ import socket
 
 ip = input("Digite o IP: ")
 
-for porta in range(1, 65535):
+for porta in range(0, 65535):
     meusocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     if meusocket.connect_ex((ip,porta)) == 0:
         print("[+] Porta", porta, "[ABERTA]")
